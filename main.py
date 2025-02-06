aca374c3c9c7e4ed69a6e106bd37bc7fe8402d2d
bhick33 <82394542+bhick33@users.noreply.github.com>
2025-02-06 06:43:08 +0000

    Start Implementing basic game flow

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from deck import Deck
 from player import Player
 
-deck = Deck()
-deck.shuffle()
-player1 = Player(deck)
 
-player1.hit()
-
-print(player1)
+def deal_cards(deck):
+    deck.shuffle()
+    p1 = Player(deck)
+    p2 = Player(deck)
+    players = [p1, p2]
+    for player in players:
+        player.hit()
+        player.hit()
     
+    return players
+
+def check_cards(p1, p2):
+    if p1.hand_value > p2.hand_value and p1.hand_value <= 21:
+        print(f"Your hand: {p1.hand_value} beats Computer's hand: {p2.hand_value}\nYou Win!")
+    elif p1.hand_value < p2.hand_value or p1.hand_value > 21:
+        print(f"Your hand: {p1.hand_value} loses to Computer's hand: {p2.hand_value}\nYou Lose!")
+    else:
+        print(f"Your hand: {p1.hand_value} and Computer's hand: {p2.hand_value}\nIt's a Draw!!")
+
+while True:
+    deck = Deck()
+
+    player1, player2 = deal_cards(deck)
+    
+    answer = input("do you want to play on?")
+    
+    if answer.lower() == 'n':
+        break
+    else:
+        print(check_cards(player1, player2))
